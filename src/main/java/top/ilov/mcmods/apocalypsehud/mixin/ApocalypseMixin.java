0a2057ca286fbf101adb935859e3dc011480a3fc
‘xhanhh’ <myxhan@outlook.com>
2025-05-30 22:37:58 +0800

    fix: 修复强制隐藏启示录重制版难度显示失效的问题

--- src/main/java/top/ilov/mcmods/apocalypsehud/mixin/ApocalypseMixin.java
+++ src/main/java/top/ilov/mcmods/apocalypsehud/mixin/ApocalypseMixin.java
@@ -1,19 +1,19 @@
 package top.ilov.mcmods.apocalypsehud.mixin;
 
-import com.toast.apocalypse.client.event.DifficultyRenderHandler;
-import net.minecraft.client.Minecraft;
-import net.minecraftforge.client.event.RenderGuiOverlayEvent;
+import com.toast.apocalypse.client.renderer.DifficultyOverlayRenderHandler;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraftforge.client.gui.overlay.ForgeGui;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import top.ilov.mcmods.apocalypsehud.ApocalypseMinimapHudMod;
 
-@Mixin(DifficultyRenderHandler.class)
+@Mixin(DifficultyOverlayRenderHandler.class)
 public class ApocalypseMixin {
 
     @Inject(method = "renderDifficulty", at = @At("HEAD"), cancellable = true)
-    private static void onRenderDifficulty(RenderGuiOverlayEvent.Post event, Minecraft minecraft, CallbackInfo ci) {
+    private static void renderDifficulty(ForgeGui gui, GuiGraphics guiGraphics, float partialTick, int width, int height, CallbackInfo ci) {
         if (!ApocalypseMinimapHudMod.CONFIG.isEnableApocalypseDisplayText()) {
             ci.cancel();
             return;
